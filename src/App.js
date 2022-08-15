09a8363cf7a3ad2b33837222132aa0faafe71a2b
Francesco Feltrinelli <44259532+ffeltrinelli@users.noreply.github.com>
2022-08-15 17:26:59 +0200

    Added subtitle and cleaned up

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
-import './App.css';
-import Text from './Text';
+import AnimatedText from './AnimatedText';
 import { Canvas } from '@react-three/fiber'
 
 const CAMERA_POSITION = [0, 0, 5]
+const EXPEDIA_BLUE = 'rgb(0, 0, 153)'
+const TITLE = 'Expedia Group'
+const SUBTITLE = 'Open Source'
 
 function App() {
   return (
-    <div className="App">
-      <Canvas camera={{ position: CAMERA_POSITION }}>
-        <pointLight position={CAMERA_POSITION} />
-        <Text text="Expedia Group"/>
-      </Canvas>
-    </div>
+    <Canvas camera={{ position: CAMERA_POSITION }}>
+      <pointLight position={CAMERA_POSITION} />
+      <AnimatedText text={TITLE} size={1.5} color={EXPEDIA_BLUE} startPosition={[-100, 1, 0]}/>
+      <AnimatedText text={SUBTITLE} size={1} color={EXPEDIA_BLUE} startPosition={[100, -1, 0]}/>
+    </Canvas>
   );
 }
 
